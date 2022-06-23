b309e0cb9836ce8b727bf960a8a412c2e3de1af8
Andyinater <48802696+Andyinater@users.noreply.github.com>
2022-06-23 12:54:31 -0400

    Add files via upload

--- OnBoard/OnBoard.ino
+++ OnBoard/OnBoard.ino
@@ -231,6 +231,16 @@ void calibrateIMU(){
 void initIMU(){
   Wire.begin(32,33); // SDA SCL
 //  delay(2000);
+  MPU9250Setting setting;
+  setting.accel_fs_sel = ACCEL_FS_SEL::A2G;
+  setting.gyro_fs_sel = GYRO_FS_SEL::G2000DPS;
+  setting.mag_output_bits = MAG_OUTPUT_BITS::M16BITS;
+  setting.fifo_sample_rate = FIFO_SAMPLE_RATE::SMPL_200HZ;
+  setting.gyro_fchoice = 0x03;
+  setting.gyro_dlpf_cfg = GYRO_DLPF_CFG::DLPF_41HZ;
+  setting.accel_fchoice = 0x01;
+  setting.accel_dlpf_cfg = ACCEL_DLPF_CFG::DLPF_45HZ;
+  
 
   if (!mpu.setup(0x68)) {  // change to your own address
       while (1) {
