672b613b1ecf1c893671f41c9d3ef63cec3c6e6f
Andyinater <48802696+Andyinater@users.noreply.github.com>
2022-06-25 08:59:45 -0400

    Add files via upload

--- KalmanTest.py
+++ KalmanTest.py
@@ -26,7 +26,7 @@ class SKF:
         self.SC = -0.08
         
         self.cf = 180.0
-        self.cr = 90.0
+        self.cr = 180.0
         
         self.m = 45.92/9.81
         self.I = 0.093
@@ -37,12 +37,16 @@ class SKF:
         
         self.state = numpy.array([[0],[0]])
         self.preState = self.state
+        self.stateHistory = []
+        self.betaHistory = []
+        
+        self.lastu = 0
         
         self.covarShape = numpy.array([[1,0],[0,1]])
         
 
-        self.Q = 0.1*self.covarShape
-        self.R = 0.1*self.covarShape
+        self.Q = 1*self.covarShape
+        self.R = 1*self.covarShape
         
         self.A = {} 
         self.B = {}
@@ -102,11 +106,25 @@ class SKF:
                 
             self.K[u] = K
             
-    def predictNextState(self, speedControl, s, measurement): # measurement is [r, ay]
+    def predictNextState(self, speedControl, steer, measurement): # measurement is [r, ay]
         # s = (-0.311/1.08)*(steer + self.SC)
-        u = round(speedControl*1.4258/0.12,2)
+        
+        s = (-0.311/1.08)*(steer + self.SC)
+        if self.lastu != 0:
+            midSpeed = (speedControl + self.lastu)/2
+            self.lastu = speedControl
+        else:
+            midSpeed = speedControl
+            
+        if midSpeed == 0:
+            midSpeed = 0.12
+        u = round(midSpeed*1.4258/0.12,2)
         
         if u == 0:
+            self.X_k1 = self.X_k1*1
+            self.beta = self.beta*1
+            self.stateHistory.append(self.X_k1)
+            self.betaHistory.append(self.beta)
             return self.X_k1
         
         v_measured = (measurement[1] - measurement[0]*u - self.A[u][0][1]*measurement[0] - self.B[u][0][0]*s)/self.A[u][0][0]
@@ -115,9 +133,14 @@ class SKF:
         
         self.X_k1 = self.X_kp1 + dot(self.K[u], numpy.array([[v_measured],[measurement[0]]]) - dot(self.Cdt[u], self.X_kp1))
         
+        
+        
         self.X_k0 = self.X_k1
         
         self.beta = self.X_k1[0]/u
+        
+        self.stateHistory.append(self.X_k1)
+        self.betaHistory.append(self.beta)
         
         return self.X_k1
     
@@ -126,16 +149,49 @@ class SKF:
         cf = csv.reader(f, delimiter = '\t')
         d = [] # [speed, steer, r, ay]
         for i in cf:
-            d.append([round(float(i[1]),2), (-0.311/1.08)*(float(i[0]) + self.SC), float(i[8])*2*numpy.pi/360.0, -1*float(i[10])])
+            dVar = lambda j:None
+            dVar.speedControl = round(float(i[1]),2)
+            dVar.steer = float(i[0])
+            dVar.t = float(i[7])
+            dVar.r = float(i[8])*2*numpy.pi/360.0
+            dVar.Ay = -1*float(i[10])
+            dVar.Ax = float(i[10])
+            
+            d.append(dVar)
         f.close()
         self.d = d
         
         stateHistory = []
         for i in d:
-            hh = self.predictNextState(i[0],i[1],[i[2],i[3]])
+            hh = self.predictNextState(i.speedControl,i.steer,[i.r, i.Ay])
             stateHistory.append([hh[0][0], hh[1][0], self.beta])
             
         return [d,stateHistory]
+    
+    def showResults(self):
+        t = []
+        Ay = []
+        r = []
+        k_v = []
+        k_r = []
+        for i in self.d:
+            t.append(i.t)
+            Ay.append(i.Ay)
+            r.append(i.r)
+            
+        for state in self.stateHistory:
+            k_v.append(state[0][0])
+            k_r.append(state[1][0])
+            
+
+            
+        plt.plot(t,Ay,'rx', label = "M_ay")
+        plt.plot(t,r,'bx', label = "M_r")
+        plt.plot(t,k_r, label = "K_r")
+        plt.plot(t, k_v, label = "K_v")
+        plt.plot(t, self.betaHistory, label = "K_b")
+        plt.grid(True)
+        plt.legend(loc = "upper left")
                 
         
        
@@ -155,7 +211,7 @@ s = round(1.4258*(6/6), 2)
 speeds = makeSpeedRange(0.01,1,0.01)
 
 testBed = SKF(speeds)
-h = testBed.simFile("RandomDrive_fixed.txt")
+h = testBed.simFile("circle_speedup_fixed.txt")
 vs = []
 rs = []
 bs = []
@@ -163,6 +219,9 @@ for i in h[1]:
     vs.append(i[0])
     rs.append(i[1])
     bs.append(i[2])
+    
+testBed.showResults()
+
 
     
     
